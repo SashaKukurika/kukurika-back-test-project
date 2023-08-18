90929e81c6a69853a8b1eee3f44aa4df8d767c56
SashaKukurika <Kukurika.sasha@gmail.com>
2023-08-18 13:23:17 +0300

    check all

--- src/cars/entities/brand.entity.ts
+++ src/cars/entities/brand.entity.ts
@@ -7,7 +7,6 @@ export class Brand {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // nullable mean that it can't be empty when it's false
   @Column({ type: 'varchar', nullable: false, unique: true })
   name: string;
 
--- src/cars/entities/car.entity.ts
+++ src/cars/entities/car.entity.ts
@@ -9,20 +9,18 @@ import {
 import { User } from '../../users/entities/user.entity';
 import { Counter } from './counter.entity';
 
-//TODO check all entity for rule
 @Entity()
 export class Car {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // nullable mean that it can't be empty when it's false
   @Column({ type: 'varchar', nullable: false })
   brand: string;
 
   @Column({ type: 'varchar', nullable: false })
   model: string;
 
-  @Column({ type: 'varchar', nullable: true })
+  @Column({ type: 'varchar', nullable: false })
   region: string;
 
   @Column({ type: 'int', nullable: false })
